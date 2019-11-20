352b1dc73a6001ba996412131e9963250e71d1e1
Musa-byte <musa.a.sakho@student.uncommonhs.org>
2019-11-20 22:35:58 +0000

    almost

--- js/script.js
+++ js/script.js
@@ -10,5 +10,20 @@
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function() {
     $("#userChoice").text($("#input").val());
+    var userChoice = $("input").val();
+    var computerChoice = Math.random();
+    if (computerChoice < 0.3){
+      $("#computerChoice").text("rock"); 
+    } else if (computerChoice > 0.3 && computerChoice < 0.6){
+        $("#computerChoice").text("paper");
+    } else if (computerChoice > 0.6){
+        $("#computerChoice").text("scissors");
+    } if (computerChoice < 0.3 && userChoice === "paper"){
+        $("#result").text("WINNER!!");
+    } else if (computerChoice > 0.3 && computerChoice < 0.6 && userChoice === "Paper"){
+        $("#result").text("WINNER!!");
+    } else if (computerChoice > 0.6 && userChoice === "paper"){
+        $("#result").text("Tie");
+    }        
 });
 
